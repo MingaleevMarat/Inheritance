f6db2f9e6754b0b57579ee23ac41066ceb3fc6ce
Ming_tc51 <Ming_tc51@itstepru.onmicrosoft.com>
2022-01-24 00:03:34 +0500

    e

--- AbstractBaseClass/main.cpp
+++ AbstractBaseClass/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 
 
+class Vehicle
+{
+public:
+	virtual void move() = 0;
 
+};
+
+class GroundVehicle : private Vehicle{};
+class Car :public GroundVehicle
+{
+public:
+	void move()
+	{
+		std::cout << "Машина едет на 4-ч колёсах" << std::endl;
+	}
+};
+
+class Bike : public GroundVehicle
+{
+public:
+	void move()
+	{
+		std::cout << "Мотоцикл едет на 2-х колёсах" << std::endl;
+	}
+};
 void main()
 {
 	setlocale(LC_ALL, " ");
+	//Vehicle v;
+	/*GroundVehicle gv;*/
+	Car BMW;
+	BMW.move();
+	Bike HarleyDavidson;
 }
