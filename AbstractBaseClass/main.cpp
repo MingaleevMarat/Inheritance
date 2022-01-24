7fd29f94f25668a6a2ce4d3b92e9675b0300fb33
Ming_tc51 <Ming_tc51@itstepru.onmicrosoft.com>
2022-01-24 20:24:41 +0500

    HomeWork

--- AbstractBaseClass/main.cpp
+++ AbstractBaseClass/main.cpp
@@ -14,7 +14,7 @@ class Car :public GroundVehicle
 public:
 	void move()
 	{
-		std::cout << "Машина едет на 4-ч колёсах" << std::endl;
+		std::cout << "Машина едет на 4-х колёсах" << std::endl;
 	}
 };
 
@@ -26,12 +26,56 @@ public:
 		std::cout << "Мотоцикл едет на 2-х колёсах" << std::endl;
 	}
 };
+
+class WaterVehicle : private Vehicle{};
+class SpeedBoat : public WaterVehicle
+{
+public:
+	void move()
+	{
+		std::cout << "Катер плывет по воде" << std::endl;
+	}
+};
+class Yacht: public WaterVehicle
+{
+public:
+	void move()
+	{
+		std::cout << "Яхта плывет по воде" << std::endl;
+	}
+};
+class AirVehicle : private Vehicle {};
+class Airplane : public AirVehicle
+{
+public:
+	void move()
+	{
+		std::cout << "Самолёт летит быстрее вертолёта(турбины)" << std::endl;
+	}
+};
+class Helicopter : public AirVehicle
+{
+public:
+	void move()
+	{
+		std::cout << "Вертолёт летит медленнее самолёта(лопости)" << std::endl;
+	}
+};
 void main()
 {
-	setlocale(LC_ALL, " ");
+	setlocale(LC_ALL, "rus");
 	//Vehicle v;
 	/*GroundVehicle gv;*/
 	Car BMW;
 	BMW.move();
 	Bike HarleyDavidson;
+	HarleyDavidson.move();
+	SpeedBoat Beneteau_Antares_9_OB;
+	Beneteau_Antares_9_OB.move();
+	Yacht Bertram_61;
+	Bertram_61.move();
+	Airplane Extra_EA_300;
+	Extra_EA_300.move();
+	Helicopter Bell_407GXP;
+	Bell_407GXP.move();
 }
